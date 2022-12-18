045a614dedfd191c6f637b08fa1d74ee6f29c0dd
space.data.robert@gmail.com <space.data.robert@gmail.com>
2022-12-18 17:53:59 +0900

    feat. 피처 선택을 위한 클래스

---
+++ source/roadmap/SelectFeature.py
@@ -0,0 +1,11 @@
+from sklearn.base import TransformerMixin, BaseEstimator
+
+class SelectFeature(BaseEstimator, TransformerMixin):
+    def __init__(self, feature_names):
+        self.feature_names = feature_names
+
+    def fit(self, x_data, y_data=None):
+        return self
+
+    def transform(self, x_data):
+        return x_data[self.feature_name].values
