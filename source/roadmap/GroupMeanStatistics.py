b4298e5f62534c54a0f88e06a1a5e036c145744d
space.data.robert@gmail.com <space.data.robert@gmail.com>
2022-12-18 18:19:18 +0900

    feat. 그룹에 따른 통계량 생성 (fit, transform)

---
+++ source/roadmap/GroupMeanStatistics.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.base import TransformerMixin, BaseEstimator
+
+
+class GroupMeanStatistics(BaseEstimator, TransformerMixin):
+    def __init__(self, feature_name, target_name=None):
+        self.feature_name = feature_name
+        self.target_name = target_name
+        self.stats_dictionary = 0
+
+    def fit(self, x_data, y_data=None):
+        self.stats_dictionary = x_data.groupby(
+            self.feature_name
+        )[self.target_name].mean().to_dict()
+        return self
+
+    def transform(self, x_data):
+        output = x_data[self.feature_name].map(
+            self.stats_dictionary
+        )
+        return output[:, None]
