8c033bfc2d9d0555f051ab8d33ddd9fa5198012d
space.data.robert@gmail.com <space.data.robert@gmail.com>
2022-12-18 18:57:49 +0900

    feat. 피처 엔지니어 함수 작성

--- 교통량 회귀 예측.py
+++ 교통량 회귀 예측.py
@@ -1,8 +1,44 @@
 from sklearn.pipeline import make_pipeline
+from sklearn.compose import make_column_transformer
+from source.common import *
+from source.roadmap import *
+
 
 def pipeline():
-    return 0
+    feature = make_column_transformer(
+        (WeatherSeasonMonth('base_date'), ['base_date']),
+        (HasLeaveMonth('base_date'), ['base_date']),
+        (HasWeekend('base_date'), ['base_date']),
+        (CategoryWeightWeekday('base_date'), ['base_date']),
+
+        (CategoryByTime('base_hour'), ['base_hour']),
+        (CycleSineHour('base_hour'), ['base_hour']),
+        (CycleCosineHour('base_hour'), ['base_hour']),
+
+        (GroupMeanStatistics('base_hour', 'target'), ['base_hour', 'target']),
+
+        # (RoadLength(['start_latitude', 'start_longitude', 'end_latitude', 'end_longitude']),
+        #  ['start_latitude', 'start_longitude', 'end_latitude', 'end_longitude']),
+
+        (DistanceFromLandmark(['start_latitude', 'start_longitude']),
+         ['start_latitude', 'start_longitude']),
+
+        (HasOneNodeName(['start_node_name', 'end_node_name']),
+         ['start_node_name', 'end_node_name']),
+
+        (HasTurnRestricted(['start_turn_restricted', 'end_turn_restricted']),
+         ['start_turn_restricted', 'end_turn_restricted']),
+
+        (StringRegexRoadName('road_name'), ['road_name']),
+
+        (CategoryAreaCluster(['start_latitude', 'start_longitude']), ['start_latitude', 'start_longitude'])
+    )
+    return feature
 
 if __name__ == '__main__':
 
-    pipeline()
+    data = read_parquet('data/road.parquet').head()
+
+    pipeline = pipeline()
+
+    print(pipeline.fit_transform(data))
