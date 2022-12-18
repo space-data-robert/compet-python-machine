a55b1ab9792c89ba59915977c242676668d48ad2
space.data.robert@gmail.com <space.data.robert@gmail.com>
2022-12-18 18:31:58 +0900

    feat. 도로의 길이를 측정 (노드 시작부터 끝)

---
+++ source/roadmap/RoadLength.py
@@ -0,0 +1,20 @@
+import haversine
+from sklearn.base import TransformerMixin, BaseEstimator
+
+
+class RoadLength(BaseEstimator, TransformerMixin):
+    def __init__(self, feature_names):
+        self.feature_names = feature_names
+
+    def fit(self, x_data, y_data=None):
+        return self
+
+    def transform(self, x_data):
+        [x_start, y_start, x_end, y_end] = self.feature_names
+
+        output = x_data.apply(
+            lambda data: haversine(
+                [data[x_start], data[y_start]],
+                [data[x_end], data[y_end]]), axis=1
+        )
+        return output[:, None]
