5b347887129b9c1d2584bd353d1957d349db19f9
space.data.robert@gmail.com <space.data.robert@gmail.com>
2022-12-19 15:57:57 +0900

    fix: 전처리 파이프라인 추가

--- 교통량 회귀 예측.py
+++ 교통량 회귀 예측.py
@@ -1,44 +1,53 @@
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import make_column_transformer
 from source.common import *
 from source.roadmap import *
 
 
-def pipeline():
-    feature = make_column_transformer(
+def pipeline(data):
+    all_features = data.columns
+
+    num_feature = make_column_transformer(
+        (CycleSineHour('base_hour'), ['base_hour']),
+        (CycleCosineHour('base_hour'), ['base_hour']),
+        (GroupMeanStatistics('base_hour', 'target'), ['base_hour', 'target']),
+        (RoadLength(['start_latitude', 'start_longitude', 'end_latitude', 'end_longitude']),
+         ['start_latitude', 'start_longitude', 'end_latitude', 'end_longitude']),
+        (DistanceFromLandmark(['start_latitude', 'start_longitude']),
+         ['start_latitude', 'start_longitude'])
+    )
+    num_pipeline = make_pipeline(
+        num_feature,
+        StandardScaler()
+    )
+
+    cate_feature = make_column_transformer(
         (WeatherSeasonMonth('base_date'), ['base_date']),
         (HasLeaveMonth('base_date'), ['base_date']),
         (HasWeekend('base_date'), ['base_date']),
         (CategoryWeightWeekday('base_date'), ['base_date']),
-
         (CategoryByTime('base_hour'), ['base_hour']),
-        (CycleSineHour('base_hour'), ['base_hour']),
-        (CycleCosineHour('base_hour'), ['base_hour']),
-
-        (GroupMeanStatistics('base_hour', 'target'), ['base_hour', 'target']),
-
-        # (RoadLength(['start_latitude', 'start_longitude', 'end_latitude', 'end_longitude']),
-        #  ['start_latitude', 'start_longitude', 'end_latitude', 'end_longitude']),
-
-        (DistanceFromLandmark(['start_latitude', 'start_longitude']),
-         ['start_latitude', 'start_longitude']),
-
         (HasOneNodeName(['start_node_name', 'end_node_name']),
          ['start_node_name', 'end_node_name']),
-
         (HasTurnRestricted(['start_turn_restricted', 'end_turn_restricted']),
          ['start_turn_restricted', 'end_turn_restricted']),
-
         (StringRegexRoadName('road_name'), ['road_name']),
-
-        (CategoryAreaCluster(['start_latitude', 'start_longitude']), ['start_latitude', 'start_longitude'])
+        (CategoryAreaCluster(['start_latitude', 'start_longitude']),
+         ['start_latitude', 'start_longitude'])
     )
-    return feature
+    cate_pipeline = make_pipeline(
+        cate_feature,
+        OneHotEncoder(categories='auto', sparse=False)
+    )
+
+    feat_pipeline = make_column_transformer(
+        (num_pipeline, all_features),
+        (cate_pipeline, all_features)
+    )
+    return feat_pipeline
 
 if __name__ == '__main__':
-
-    data = read_parquet('data/road.parquet').head()
-
-    pipeline = pipeline()
-
-    print(pipeline.fit_transform(data))
+    data = read_parquet('data/road.parquet')
+    pipeline = pipeline(data)
