1bceaa4a75b35242d24d2182825489aeeb0c650d
space.data.robert@gmail.com <space.data.robert@gmail.com>
2023-01-02 18:08:52 +0900

    fix. 컨셉 시프트 모델 구현

--- 제주도 교통량 모델링.py
+++ 제주도 교통량 모델링.py
@@ -1,54 +1,94 @@
+import warnings
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import make_column_transformer
+import catboost as cat
+import optuna
+warnings.filterwarnings('ignore')
+
 from source.base import *
 from source.roadmap import *
 
 
-def pipeline(data):
-    all_features = data.columns
+def pipeline(x_data, y_data):
+    all_features = x_data.columns
 
-    num_feature = make_column_transformer(
-        (SelectFeature('maximum_speed_limit'), ['maximum_speed_limit']),
-        (CycleSineHour('base_hour'), ['base_hour']),
-        (CycleCosineHour('base_hour'), ['base_hour']),
-        (GroupMeanStatistics('base_hour', 'target'), ['base_hour', 'target']),
-        (RoadLength(['start_latitude', 'start_longitude', 'end_latitude', 'end_longitude']),
-         ['start_latitude', 'start_longitude', 'end_latitude', 'end_longitude']),
-        (DistanceFromLandmark(['start_latitude', 'start_longitude']),
-         ['start_latitude', 'start_longitude'])
+    _nummeries = make_column_transformer(
+        (CycleSineHour('base_hour'), all_features),
+        (CycleCosineHour('base_hour'), all_features),
+        (RoadLength(['start_latitude', 'start_longitude', 'end_latitude', 'end_longitude']), all_features),
+        (DistanceFromLandmark(['start_latitude', 'start_longitude']), all_features),
+        # (GroupMeanStatistics('base_hour'), all_features),
     )
-    num_pipeline = make_pipeline(
-        num_feature,
+    nummeries = make_pipeline(
+        _nummeries,
+        SimpleImputer(strategy='most_frequent'),
         StandardScaler()
     )
 
-    cate_feature = make_column_transformer(
-        (WeatherSeasonMonth('base_date'), ['base_date']),
-        (HasLeaveMonth('base_date'), ['base_date']),
-        (HasWeekend('base_date'), ['base_date']),
-        (CategoryWeightWeekday('base_date'), ['base_date']),
-        (CategoryByTime('base_hour'), ['base_hour']),
-        (HasOneNodeName(['start_node_name', 'end_node_name']),
-         ['start_node_name', 'end_node_name']),
-        (HasTurnRestricted(['start_turn_restricted', 'end_turn_restricted']),
-         ['start_turn_restricted', 'end_turn_restricted']),
-        (StringRegexRoadName('road_name'), ['road_name']),
-        (CategoryAreaCluster(['start_latitude', 'start_longitude']),
-         ['start_latitude', 'start_longitude'])
+    _categories = make_column_transformer(
+        (WeatherSeasonMonth('base_date'), all_features),
+        (HasLeaveMonth('base_date'), all_features),
+        (HasWeekend('base_date'), all_features),
+        (CategoryWeightWeekday('base_date'), all_features),
+        (CategoryByTime('base_hour'), all_features),
+        (HasOneNodeName(['start_node_name', 'end_node_name']), all_features),
+        (HasTurnRestricted(['start_turn_restricted', 'end_turn_restricted']), all_features),
+        (StringRegexRoadName('road_name'), all_features),
+        (CategoryAreaCluster(['start_latitude', 'start_longitude']), all_features)
     )
-    cate_pipeline = make_pipeline(
-        cate_feature,
+    categories = make_pipeline(
+        _categories,
+        SimpleImputer(strategy='most_frequent'),
         OneHotEncoder(categories='auto', sparse=False)
     )
 
-    feat_pipeline = make_column_transformer(
-        (num_pipeline, all_features),
-        (cate_pipeline, all_features)
+    features = make_column_transformer(
+        (nummeries, all_features),
+        (categories, all_features)
     )
-    return feat_pipeline
+
+    _pipeline = make_pipeline(
+        features,
+        cat.CatBoostRegressor(random_state=27, verbose=False)
+    )
+
+    model = 'catboostregressor'
+
+    params = {
+        f'{model}__learning_rate': optuna.distributions.LogUniformDistribution(0.01, 0.3),
+    }
+    search = optuna.integration.OptunaSearchCV(
+        _pipeline,
+        params,
+        cv=3,
+        n_trials=5,
+        timeout=600,
+        scoring='neg_root_mean_squared_error',
+    )
+    search.fit(x_data, y_data)
+
+    score = search.best_score_
+    print(f'best score = {-score: .5f}')
+
+    return search
+
 
 if __name__ == '__main__':
     data = read_parquet('data/road.parquet')
-    pipeline = pipeline(data)
+
+    categories = sorted(
+        data.maximum_speed_limit.unique()
+    )
+    for category in categories:
+        x_train = data[
+            data.maximum_speed_limit == category
+        ].copy()
+
+        y_train = x_train.pop('target')
+
+        predictor = pipeline(x_train, y_train)
+
+        # predictor.predict(x_train)
