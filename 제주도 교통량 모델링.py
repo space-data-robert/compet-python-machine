a44e89ab1ac3af1c41cc1ecf35b50a2159b7f661
space.data.robert@gmail.com <space.data.robert@gmail.com>
2023-01-04 19:29:05 +0900

    fix. 컨셉 시프트 모델 구현

--- 제주도 교통량 모델링.py
+++ 제주도 교통량 모델링.py
@@ -1,16 +1,7 @@
-import warnings
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import make_pipeline
-from sklearn.compose import make_column_transformer
-<<<<<<< HEAD
 import catboost as cat
 import optuna
 warnings.filterwarnings('ignore')
 
-=======
->>>>>>> develop
 from source.base import *
 from source.roadmap import *
 
