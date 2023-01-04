865f1f47f5581905906d8445f7fd87d6f97da105
space.data.robert@gmail.com <space.data.robert@gmail.com>
2023-01-04 19:30:50 +0900

    feat. SPG 번호를 의미에 맞게 분리

---
+++ source/logistic/SplitSPGNumber.py
@@ -0,0 +1,35 @@
+from sklearn.base import TransformerMixin, BaseEstimator
+from typing import List
+
+
+class SplitSPGNumber(BaseEstimator, TransformerMixin):
+    def __init__(self, feature_name):
+        self.feature_name = feature_name
+        self.categories: List[int] = [0, 4, 9, 10, 15]
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        outout = X[[self.feature_name]].copy().astype(str)
+
+        if (self.feature_name).startswith('REC'):
+            for ind in range(16):
+                outout[f'{self.feature_name}{ind}'] = outout[self.feature_name].apply(
+                    lambda x: int(x[ind])
+                ).astype('category')
+            outout.drop(
+                self.feature_name, axis=1, inplace=True)
+
+        if (self.feature_name).startswith('SEND'):
+            for ind in range(len(self.categories) - 1):
+                outout[f'{self.feature_name}{ind}'] = outout[self.feature_name].apply(
+                    lambda x: sum(
+                        list(map(int, x[self.categories[ind]: self.categories[ind + 1]]))
+                    )
+
+                ).astype('category')
+            outout.drop(
+                self.feature_name, axis=1, inplace=True)
+
+        return outout
