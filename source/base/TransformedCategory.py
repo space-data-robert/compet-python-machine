4ddbacb8fded454a8741843a133944893edd7365
space.data.robert@gmail.com <space.data.robert@gmail.com>
2023-01-04 19:31:37 +0900

    feat. 피처를 선택하고 카테고리 타입으로 반환

---
+++ source/base/TransformedCategory.py
@@ -0,0 +1,12 @@
+from sklearn.base import TransformerMixin, BaseEstimator
+
+
+class TransformedCategory(BaseEstimator, TransformerMixin):
+    def __init__(self, feature_names):
+        self.feature_names = feature_names
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[self.feature_names].astype('category')
