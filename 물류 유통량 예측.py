214589f5df7c392f4a84b4e63bc35b33891893b6
space.data.robert@gmail.com <space.data.robert@gmail.com>
2023-01-04 19:36:44 +0900

    create. 유통량 예측 베이스라인 모델

---
+++ 물류 유통량 예측.py
@@ -0,0 +1,13 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+from source.base import *
+from source.logistic import *
+
+
+def pipeline():
+    return 0
+
+
+if __name__ == '__main__':
+    print()
