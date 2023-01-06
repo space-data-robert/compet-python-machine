24b077d0b1eae4b954de6edfc7fd02815bee3d96
space-data-robert <space.data.robert@gmail.com>
2023-01-06 15:38:42 +0900

    feat. 모듈 경로 추가 - 정규분포로 변환과 복구

--- source/base/__init__.py
+++ source/base/__init__.py
@@ -1,4 +1,6 @@
+""" Set the path for class call convenience. """
 from .MakeParquet import *
 from .ReadData import *
 from .SelectFeature import *
 from .TransformedCategory import *
+from .CustomPowerTransformer import *
