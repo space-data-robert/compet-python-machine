2006344583b2962bdc34553e792e132662d13d68
space-data-robert <space.data.robert@gmail.com>
2023-01-11 13:36:40 +0900

    feat. 주차 수요량 예측 모델링 (예정)

--- 주차 수요량 예측.py
+++ 주차 수요량 예측.py
@@ -1,8 +1,28 @@
+import sys
+import warnings
+import numpy as np
 import pandas as pd
+warnings.filterwarnings('ignore')
+pd.set_option('display.max_columns', None)
+
+import sklearn.neighbors._base
+sys.modules['sklearn.neighbors.base'] = sklearn.neighbors._base
+from missingpy import MissForest
+
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import make_column_transformer
 
 
 if __name__ == '__main__':
 
     data: pd.DataFrame = pd.read_parquet('data/parking.parquet')
-
     print(f'data.shape = {data.shape}')
+
+    target_name: str = 'RegisteredVehicleCount'
+
+    x_train = data.copy()
+    y_train = x_train.pop(target_name)
+
+
